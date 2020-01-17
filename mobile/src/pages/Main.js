8053538675a9b4d953eeefd1066ce605688aeeb1
Thiago Teodoro <thiago.teodoro.rodrigues@gmail.com>
2020-01-16 23:02:40 -0300

    Salvando progresso da aula de Mobile.

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import MapView from 'react-native-maps';
+
+function Main() {
+    return <MapView style={{flex: 1}} />
+}
+
+export default Main;
